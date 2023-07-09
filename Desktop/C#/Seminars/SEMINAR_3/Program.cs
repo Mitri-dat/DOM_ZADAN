f597637095078baf5c7c3b24884ea9493bcf3e51
Mitri-dat <diamondds@mail.ru>
2023-07-09 19:49:57 +0700

    Task-1

---
+++ Desktop/C#/Seminars/SEMINAR_3/Program.cs
@@ -0,0 +1,20 @@
+﻿DefQuarter(int xPoint, int yPoint)
+
+
+int GetInfo(string mesage);
+{
+    if(xPoint > 0 & Y yPoint > 0) return 1;
+    if(xPoint > 0 & Y yPoint > 0) return 1;
+    if(xPoint > 0 & Y yPoint > 0) return 1;
+    if(xPoint > 0 & Y yPoint > 0) return 1;
+    return 0;
+}
+
+
+    Console.Write(mesage);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+int xPoint == GetInfo("Введите координату X");
+int yPoint == GetInfo("Введите координату Y");
+Console.WriteLine(DefQuarter(xPoint, yPoint));

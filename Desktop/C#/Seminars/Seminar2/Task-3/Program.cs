42cf76722400571fbbe655818c035fb5f2be94ef
Dmiriy Datsyuk <diamondds@mail.ru>
2023-07-13 21:39:18 +0700

    task-2

---
+++ Desktop/C#/Seminars/Seminar2/Task-3/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Написать программу которая на вход принимает два числа и определяет, является ли второе, кратным первому
+// например: 34, 5 -> не кратно, остаток 4 
+
+Console.WriteLine("Введите первое число : ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int number2 = int.Parse(Console.ReadLine());
+
+int ost = number1 % number2;
+if (ost > 0 )Console.WriteLine($" Остаток {ost}");
+    else  Console.WriteLine($" {number1}, {number2} кратно");  
+ 

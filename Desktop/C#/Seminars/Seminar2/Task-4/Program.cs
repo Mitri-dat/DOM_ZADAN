8f1693a380213b510fe4947edad5b9dcc4da6b36
Dmiriy Datsyuk <diamondds@mail.ru>
2023-07-14 21:12:35 +0700

    task-4

---
+++ Desktop/C#/Seminars/Seminar2/Task-4/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Задача 4. Написать программу которая на вход принимает  число и определяет, является ли оно кратным и 7 и 23.
+// например: 161 -> Да
+
+Console.WriteLine("Введите первое число : ");
+int number = int.Parse(Console.ReadLine());
+
+int ost1 = number % 7;
+int ost2 = number % 23;
+
+if (ost1 == 0 && ost2 == 0 )Console.WriteLine($" {number}, да");
+    else  Console.WriteLine($" {number}, нет");

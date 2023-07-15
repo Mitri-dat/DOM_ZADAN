e5da9aef3e38cb1427f4d66b672f2e524abc4673
Dmiriy Datsyuk <diamondds@mail.ru>
2023-07-15 11:50:49 +0700

    расстояние между точками

---
+++ Desktop/C#/Seminars/Seminar3/Task-3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает координаты двух точек и определяет расстояние между ними
+
+
+double lenght(int xA, int yA, int xB, int yB) 
+{
+    double distance = Math.Sqrt(Math.Pow(xB - xA, 2)+Math.Pow(yB - yA, 2));
+    return distance;
+}
+
+int GetInfo(string mesage)
+{
+    Console.Write(mesage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+int coorA1 = GetInfo ("Ведите координату Х точки А:");
+int coorA2 = GetInfo ("Ведите координату У точки А:");
+int coorB1 = GetInfo ("Ведите координату Х точки В:");
+int coorB2 = GetInfo ("Ведите координату У точки В:");
+
+Console.WriteLine($"Расстояние между точками на плоскости равно: {lenght(coorA1, coorB1 ,coorA2, coorB2)}");

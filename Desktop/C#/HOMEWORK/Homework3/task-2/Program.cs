1efa20fb8df26fa1021130dfe6cf78bce81e1488
Dmiriy Datsyuk <diamondds@mail.ru>
2023-07-16 11:15:24 +0700

    task-2

---
+++ Desktop/C#/HOMEWORK/Homework3/task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+double lenght(int xA, int yA, int zA, int xB, int yB, int zB) 
+{
+    double distance = Math.Sqrt(Math.Pow(xB - xA, 2)+Math.Pow(yB - yA, 2)+Math.Pow(zB - yA, 2));
+    return distance;
+}
+
+int GetInfo(string mesage)
+{
+    Console.Write(mesage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+int coorA1 = GetInfo ("Ведите координату Х точки А:");
+int coorA2 = GetInfo ("Ведите координату У точки А:");
+int coorA3 = GetInfo ("Ведите координату z точки А:");
+int coorB1 = GetInfo ("Ведите координату Х точки В:");
+int coorB2 = GetInfo ("Ведите координату У точки В:");
+int coorB3 = GetInfo ("Ведите координату z точки B:");
+
+Console.WriteLine($"Расстояние между точками на плоскости равно: {lenght(coorA1, coorB1 ,coorA2, coorB2, coorA3, coorB3)}");
---
+++ Desktop/C#/Seminars/Seminar4/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int FindFactorial(int num)
+{
+    int faktorial = 1;
+    while (num > 0)
+    {
+        faktorial = faktorial * num;
+        num--;
+    }
+    return faktorial;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число: ");
+int faktorial = FindFactorial(num);
+Console.WriteLine(faktorial);

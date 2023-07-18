556eaf536386a2c810d6b390e61843206406f3a6
Dmiriy Datsyuk <diamondds@mail.ru>
2023-07-18 23:01:23 +0700

    task-1 Массив сложение вычитание, bool

---
+++ Desktop/C#/Seminars/Seminar5/task-1/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Clear();
+
+int GetPosOrNegativeSum(int[] array, bool posOrNegative)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (posOrNegative)
+        {
+            if (array[i] > 0)
+            {
+                result += array[i];
+            }
+        }
+        else
+        {
+            if (array[i] < 0)
+            {
+                result += array[i];
+            }
+        }
+
+    }
+    return result;
+}
+int[] MakeNewRandomArray(int size, int beginValue, int EndWalue)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+            array[i] = new Random().Next(beginValue, EndWalue);
+        return array;
+    }
+
+int[] array = MakeNewRandomArray(12, -9, 9);
+
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine($"Сумма положительных чисел равна{GetPosOrNegativeSum(array,true)}"); // Console.WriteLine("[" + string.Join(",",array) + "]");
+Console.WriteLine($"Сумма отрицательных чисел равна{GetPosOrNegativeSum(array,false)}");
+
+
+
+// int positiveSum = 0;
+// int negativeSum = 0;
+
+// while (array[int++])
+// {
+//     if
+// }
+
+
+
+//  Console.WriteLine($"сумма положительных числ равна {GetPosOrNegativeSum}")
+
+
+
+
+
+
+
